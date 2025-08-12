290cd7d13dc4df8f385bbd425e591b9ef89a59dd
Nitin Singh <nitinsingh@Nitins-MacBook-Pro.local>
2025-08-13 00:10:20 +0530

    Fix server URL to point to correct Vercel deployment

--- src/utils/server.ts
+++ src/utils/server.ts
@@ -2,4 +2,6 @@ const dev = process.env.NODE_ENV !== "production";
 
 export const server = dev
   ? "http://localhost:3000"
-  : "https://next-ecommerce-front.vercel.app";
+  : process.env.VERCEL_URL 
+    ? `https://${process.env.VERCEL_URL}`
+    : "https://perfume-ecom-4wlaaoy0m-nitins-projects-51fde495.vercel.app";
