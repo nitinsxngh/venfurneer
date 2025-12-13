01eaed44d73e94e6dc216ed0100afdfdaf8db776
Nitin Singh <nitinsingh@Nitins-MacBook-Pro.local>
2025-12-13 18:22:59 +0530

    Use Vercel's recommended patched versions for CVE fix
    - Next.js: 15.0.7 (patched for CVE-2025-66478)
    - React: 18.3.1 (patched for CVE-2025-55182)
    - Restore React import for React 18 compatibility
    - Matches Vercel's automated PR recommendations

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,7 +8,7 @@ import "react-rater/lib/react-rater.css";
 import type { AppProps } from "next/app";
 import { Poppins } from "next/font/google";
 import Router from "next/router";
-import { Fragment } from "react";
+import React, { Fragment } from "react";
 import { SWRConfig } from "swr";
 
 import { wrapper } from "../store";
